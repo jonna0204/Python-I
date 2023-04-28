3b669235d1e72848a6a9d47af06866bf87fce62e
jonna0204 <130108957+jonna0204@users.noreply.github.com>
2023-04-28 12:17:27 -0400

    Update evaluacion.py

--- S7/evaluacion.py
+++ S7/evaluacion.py
@@ -1,5 +1,4 @@
 import time
-
 lista = [[1,2,3], [0,4,5], [4,0,1], [6,5,4]]
 for i in lista:
     if i[0] == 0: #SI EL PRIMER NUMERO DE LA LISTA ES 0 SE CONTINUA A LA SIGUIENTE.
@@ -7,9 +6,9 @@ for i in lista:
     print(i)
     time.sleep(0.5)
 
-for i in lista:      
-    for j in i:
-        if j == 0:#se quita el 0 de los valores
-            continue
-        print(j)#se imprimen los valores de la sublista
-        time.sleep(0.5)
+for index in lista: #se recorre nuevamente la lista
+    for j in index: #se recorre el index
+        if j == 0: #si dentro del index hay un numero 0
+            index.remove(0) #se elimina el numero
+    print(index)
+    time.sleep(0.5)
